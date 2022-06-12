c91691c1db5e475de15eef4ffa7b0e52b83609c9
kota-yata <kota@yatagai.com>
2022-06-11 23:20:10 -0500

    Modify port setting

--- src/main/java/wsjava/signaling/SignalingApplication.java
+++ src/main/java/wsjava/signaling/SignalingApplication.java
@@ -10,10 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SignalingApplication {
 
 	public static void main(String[] args) {
-    // SpringApplication app = new SpringApplication(SignalingApplication.class);
-    // app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
-    // app.run(args);
-		SpringApplication.run(SignalingApplication.class, args);
+    SpringApplication app = new SpringApplication(SignalingApplication.class);
+    app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
+    app.run(args);
+		// SpringApplication.run(SignalingApplication.class, args);
 	}
 
 }
